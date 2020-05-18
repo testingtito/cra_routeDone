1ceb36d342f770e3799c56a4468196f29cf8c71f
Toby Chee <wizardofqa@outlook.com>
2020-05-17 22:25:36 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import Header from './components/Header';
+import HomeGuest from './components/HomeGuest';
+import Footer from './components/Footer';
+import About from './components/About';
+import Terms from './components/Terms';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Footer />
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
