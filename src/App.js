4ca0dc4c2e8899d20e3b8e992b1286e3f1d3d6cd
Toby Chee <wizardofqa@outlook.com>
2020-05-24 08:09:41 -0700

    Route has been added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import HomeGuest from './components/HomeGuest';
 import Footer from './components/Footer';
 import About from './components/About';
 import Terms from './components/Terms';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
+      <Switch>
+        <Route path='/' component='' exact>
+          <HomeGuest />
+        </Route>
+        <Route path='/about-us'>
+          <About />
+        </Route>
+        <Route path='/terms'>
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
 
-
-    </div>
+    </BrowserRouter>
   )
 }
 
